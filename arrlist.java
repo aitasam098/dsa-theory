90392e5ece351f04576cff1cc3c65a8431a885db
aitasam098 <aitahuss1234@gmail.com>
2025-01-12 21:17:09 +0500

    Add files via upload

---
+++ arrlist.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class arrlist {
+    static class stack{
+        static ArrayList<Integer> list = new ArrayList<>();
+        public static boolean isEmpty(){
+            return list.size()==0;
+        }
+        //push
+        public static  void push(int data){
+        list.add(data);
+        }
+        //pop
+        public static int pop(){
+            if (isEmpty()) {
+                return -1;
+            }
+            int top=list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
+        public static int peek(){
+            if (isEmpty()){
+                return -1;
+            }
+            return list.get(list.size()-1);
+        }
+    }
+    public static void AddAtBottom(int data,Stack<Integer> s){
+        
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top=s.pop();
+        AddAtBottom(data,s);
+        s.push(top);
+    }
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+
+        AddAtBottom(45,s);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+           
+            
+        }
+    }
+    
+}
